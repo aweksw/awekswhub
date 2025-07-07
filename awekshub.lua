3472d62faa9d39ab392d0bb46d6b9564add7f1c0
aweksw <rumtur56@bk.ru>
2025-07-07 12:52:10 +0300

    Create awekshub.lua

---
+++ awekshub.lua
@@ -0,0 +1,142 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+
+local Window = Library.CreateLib("aweks hub", "RJTheme6")
+
+local Tab = Window:NewTab("Multi")
+
+local Section = Tab:NewSection("TP")
+
+Section:NewButton("seed shop", "телепортирует к магазину", function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Tutorial_Points.Tutorial_Point_1.CFrame
+end)
+
+local Section = Tab:NewSection("Player")
+
+Section:NewToggle("low gravity", "низкая гравитация", function(state)
+    if state then
+        workspace.Gravity = 40
+    else
+        workspace.Gravity = 192
+    end
+end)
+
+Section:NewTextBox("SpeedHack", "изменяет скорость ходьбы", function(txt)
+ game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = txt
+end)
+
+Section:NewTextBox("JumpHack", "Изменяет силу прыжка", function(txt)
+ game.Players.LocalPlayer.Character.Humanoid.JumpPower = txt
+end)
+
+local Section = Tab:NewSection("misc")
+
+Section:NewButton("ESP", "показывает игроков через стены", function()
+for i, childrik in ipairs(workspace:GetDescendants()) do
+        if childrik:FindFirstChild("Humanoid") then
+            if not childrik:FindFirstChild("EspBox") then
+                if childrik ~= game.Players.LocalPlayer.Character then
+                    local esp = Instance.new("BoxHandleAdornment",childrik)
+                    esp.Adornee = childrik
+                    esp.ZIndex = 0
+                    esp.Size = Vector3.new(4, 5, 2)
+                    esp.Transparency = 0.65
+                    esp.Color3 = Color3.fromRGB(45,255,48)
+                    esp.AlwaysOnTop = true
+                    esp.Name = "EspBox"
+                end
+            end
+        end
+    end
+end)
+local Section = Tab:NewSection("Shop")
+
+
+
+Section:NewButton("buy shop", "купить все семена", function()
+while wait(0.3) do
+local args = {
+	"Carrot"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuySeedStock"):FireServer(unpack(args))
+local args = {
+	"Strawberry"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuySeedStock"):FireServer(unpack(args))
+local args = {
+	"Orange Tulip"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuySeedStock"):FireServer(unpack(args))
+local args = {
+	"Blueberry"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuySeedStock"):FireServer(unpack(args))
+local args = {
+	"Tomato"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuySeedStock"):FireServer(unpack(args))
+local args = {
+	"Watermelon"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuySeedStock"):FireServer(unpack(args))
+local args = {
+	"Daffodil"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuySeedStock"):FireServer(unpack(args))
+local args = {
+	"Pumpkin"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuySeedStock"):FireServer(unpack(args))
+local args = {
+	"Apple"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuySeedStock"):FireServer(unpack(args))
+local args = {
+	"Bamboo"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuySeedStock"):FireServer(unpack(args))
+local args = {
+	"Cactus"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuySeedStock"):FireServer(unpack(args))
+local args = {
+	"Dragon Fruit"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuySeedStock"):FireServer(unpack(args))
+local args = {
+	"Mango"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuySeedStock"):FireServer(unpack(args))
+local args = {
+	"Grape"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuySeedStock"):FireServer(unpack(args))
+local args = {
+	"Mushroom"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuySeedStock"):FireServer(unpack(args))
+local args = {
+	"Pepper"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuySeedStock"):FireServer(unpack(args))
+local args = {
+	"Cacao"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuySeedStock"):FireServer(unpack(args))
+local args = {
+	"Beanstalk"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuySeedStock"):FireServer(unpack(args))
+local args = {
+	"Ember Lily"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuySeedStock"):FireServer(unpack(args))
+local args = {
+	"Sugar Apple"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuySeedStock"):FireServer(unpack(args))
+local args = {
+	"Burning Bud"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuySeedStock"):FireServer(unpack(args))
+end
+end)
